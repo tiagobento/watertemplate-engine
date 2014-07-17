d64f33b18010a6a8025c860119b431c04b6b992c
Tiago Bento <tiagobentofernandes@gmail.com>
2014-07-17 05:08:12 -0300

    Added initializer of productions list in NonTerminal to create productions only once.

--- watertemplate-engine/src/main/java/org/watertemplate/interpreter/parser/NonTerminal.java
+++ watertemplate-engine/src/main/java/org/watertemplate/interpreter/parser/NonTerminal.java
@@ -9,39 +9,39 @@ enum NonTerminal implements ParserSymbol {
 
     IF_COMMAND {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(IF, ID, STATEMENTS, ELSE_BLOCK, END));
         }
     },
     FOR_COMMAND {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(FOR, PROPERTY_NAME, IN, ID, STATEMENTS, ELSE_BLOCK, END));
         }
     },
     ELSE_BLOCK {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(ELSE, STATEMENTS));
             productions.add(rhs());
         }
     },
     ID {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(PROPERTY_NAME, ACCESSOR, ID));
             productions.add(rhs(PROPERTY_NAME));
         }
     },
     ID_EVALUATION {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(ID));
         }
     },
     STATEMENT {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(TEXT));
             productions.add(rhs(FOR_COMMAND));
             productions.add(rhs(IF_COMMAND));
@@ -50,31 +50,33 @@ enum NonTerminal implements ParserSymbol {
     },
     STATEMENTS {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(STATEMENT, STATEMENTS));
             productions.add(rhs());
         }
     },
     START_SYMBOL {
         @Override
-        void productions(final List<Production> productions) {
+        void addProductions(final List<Production> productions) {
             productions.add(rhs(STATEMENTS, END_OF_INPUT));
         }
     };
 
-    abstract void productions(final List<Production> productions);
-
-    public final List<Production> productions() {
-        final List<Production> productions = new ArrayList<>();
-        productions(productions);
-        return productions;
+    static {
+        for (NonTerminal nonTerminal : NonTerminal.values()) {
+            nonTerminal.addProductions(nonTerminal.productions);
+        }
     }
+
+    private final List<Production> productions = new ArrayList<>();
 
     private static Production rhs(final ParserSymbol... symbols) {
         return new Production(symbols);
     }
 
     public final boolean matches(final TokenStream tokenStream) {
-        return productions().stream().anyMatch((production) -> production.matches(tokenStream));
+        return productions.stream().anyMatch((production) -> production.matches(tokenStream));
     }
+
+    abstract void addProductions(final List<Production> productions);
 }
