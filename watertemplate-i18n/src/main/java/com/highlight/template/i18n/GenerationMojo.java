92eedb0a71b719393ce357a1db2dd98e509105fa
Tiago Bento <tiagobentofernandes@gmail.com>
2015-04-14 19:03:48 +0200

    Updated Maven Plugin API.

--- watertemplate-i18n/src/main/java/com/highlight/template/i18n/GenerationMojo.java
+++ watertemplate-i18n/src/main/java/com/highlight/template/i18n/GenerationMojo.java
@@ -3,30 +3,20 @@ package com.highlight.template.i18n;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
-/**
- * @goal generate
- * @phase process-resources
- */
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
 public class GenerationMojo extends AbstractMojo {
 
-    /* //////////////////////////////////////////////////////////////////////////////////// */
-    /* Do not remove these Javadocs. Maven uses them to inject values and detect properties *
-    /* //////////////////////////////////////////////////////////////////////////////////// */
-
-    /**
-     * @parameter
-     */
+    @Parameter(name = "baseDir")
     private String baseDir;
 
-    /**
-     * @parameter
-     */
+    @Parameter(name = "destinationDir")
     private String destinationDir;
 
-    /**
-     * @parameter
-     */
+    @Parameter(name = "bundlesDir")
     private String bundlesDir;
 
     @Override
