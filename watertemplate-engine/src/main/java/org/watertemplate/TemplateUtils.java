37bd51aa884529b6515920637f17387634b1c761
Tiago Bento <tiagobentofernandes@gmail.com>
2015-04-22 12:49:02 +0200

    Improvements on String creation from Template stream

--- watertemplate-engine/src/main/java/org/watertemplate/TemplateUtils.java
+++ watertemplate-engine/src/main/java/org/watertemplate/TemplateUtils.java
@@ -5,9 +5,8 @@ import java.util.stream.Stream;
 
 public class TemplateUtils {
     public static String buildString(final Stream<Supplier<String>> supplierStream) {
-        return supplierStream.parallel()
-                .map(Supplier::get)
-                .reduce(String::concat)
-                .orElse("");
+        return supplierStream
+                .parallel()
+                .reduce("", (s, supplier) -> s + supplier.get(), String::concat);
     }
 }
